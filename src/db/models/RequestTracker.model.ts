44d426f9ed460f1cb2fefa7d64aa43fd5da42bb3
baha-nouaili <b.noueili@gmail.com>
2023-06-04 18:11:51 +0100

    fix: referring to mongoose docs

--- src/db/models/RequestTracker.model.ts
+++ src/db/models/RequestTracker.model.ts
@@ -5,35 +5,28 @@ import {
   RequestTrackerPayload,
 } from "../../interface/RequestTracker.interfaces";
 
-export const requestTrackerSchema = new mongoose.Schema(
-  {
-    userId: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    requestCount: {
-      type: Number,
-      required: true,
-      default: 0,
-    },
-    lastActivity: {
-      type: Number,
-      required: true,
-    },
+export const requestTrackerSchema = new mongoose.Schema({
+  userId: {
+    type: String,
+    required: true,
+    unique: true,
   },
-  {
-    toJSON: {
-      transform(doc, ret, options) {
-        (ret.id = ret._id), delete ret._id, delete ret.__v;
-      },
-    },
-  }
-);
+  requestCount: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  lastActivity: {
+    type: Number,
+    required: true,
+  },
+});
 
 // could be placed in a repo layer later
-requestTrackerSchema.statics.insertOne = (payload: RequestTrackerPayload) => {
-  return RequestTracker.create(payload);
+requestTrackerSchema.statics.insertOne = function (
+  payload: RequestTrackerPayload
+) {
+  return this.create(payload);
 };
 
 const RequestTracker = mongoose.model<
