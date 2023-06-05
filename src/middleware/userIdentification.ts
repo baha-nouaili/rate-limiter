fd99636e4b2d34a39734fd23b7499769a3e7d8db
baha-nouaili <b.noueili@gmail.com>
2023-06-05 10:10:11 +0100

    using request-ip package to get the real user's ip

--- src/middleware/userIdentification.ts
+++ src/middleware/userIdentification.ts
@@ -1,12 +1,13 @@
 import { NextFunction, Request, Response } from "express";
 import { address } from "ip";
+import { getClientIp } from "request-ip";
 
 export const userIdentificationMiddleware = (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  const userId = req.headers.me || address();
+  const userId = req.headers.me || getClientIp(req) || address();
   req.body.userId = userId;
   next();
 };
